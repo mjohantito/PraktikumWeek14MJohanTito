bbd0fbd42bcfcd1d8e34410d7f1ae826e8bba3d0
manuel tito <manueljohantito12@gmail.com>
2020-12-18 10:10:18 +0700

    PraktikumWeek14MJohanTito

---
+++ PraktikumWeek14_reversesort/PraktikumWeek14_reversesort/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PraktikumWeek14_reversesort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonReverse = new System.Windows.Forms.Button();
+            this.buttonUrut = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INPUT : ";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(70, 73);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(168, 20);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // buttonReverse
+            // 
+            this.buttonReverse.Location = new System.Drawing.Point(70, 135);
+            this.buttonReverse.Name = "buttonReverse";
+            this.buttonReverse.Size = new System.Drawing.Size(75, 23);
+            this.buttonReverse.TabIndex = 2;
+            this.buttonReverse.Text = "REVERSE";
+            this.buttonReverse.UseVisualStyleBackColor = true;
+            this.buttonReverse.Click += new System.EventHandler(this.buttonReverse_Click);
+            // 
+            // buttonUrut
+            // 
+            this.buttonUrut.Location = new System.Drawing.Point(179, 135);
+            this.buttonUrut.Name = "buttonUrut";
+            this.buttonUrut.Size = new System.Drawing.Size(75, 23);
+            this.buttonUrut.TabIndex = 3;
+            this.buttonUrut.Text = "URUTKAN";
+            this.buttonUrut.UseVisualStyleBackColor = true;
+            this.buttonUrut.Click += new System.EventHandler(this.buttonUrut_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutput.Location = new System.Drawing.Point(70, 265);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(71, 25);
+            this.labelOutput.TabIndex = 4;
+            this.labelOutput.Text = "Output";
+            this.labelOutput.Click += new System.EventHandler(this.labelOutput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonUrut);
+            this.Controls.Add(this.buttonReverse);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonReverse;
+        private System.Windows.Forms.Button buttonUrut;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ PraktikumWeek14_reversesort/PraktikumWeek14_reversesort/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PraktikumWeek14_reversesort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReverse_Click(object sender, EventArgs e)
+        {
+            string tempatreverse = "";
+            if (textBoxInput.Text == String.Empty)
+            {
+                MessageBox.Show("Tolong isi text box terlebih dahulu");
+                labelOutput.Text = "Output";
+            }
+            else
+            {
+                labelOutput.Text = textBoxInput.Text;
+                for (int i = textBoxInput.TextLength - 1; i >= 0; i--)
+                {
+                    tempatreverse += textBoxInput.Text[i];
+                }
+                labelOutput.Text = tempatreverse;
+            }
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonUrut_Click(object sender, EventArgs e)
+        {
+            
+            if (textBoxInput.Text == String.Empty)
+            {
+                MessageBox.Show("Tolong isi text box terlebih dahulu");
+                labelOutput.Text = "Output";
+            }
+            else
+            {
+                string input1 = textBoxInput.Text;
+                char[] sort = input1.ToCharArray();
+                //string listsymbol = "`~!@#$%^&*()_+-=[]{}|;':[]<>?,./";
+                //char[] symbol1 = listsymbol.ToCharArray();
+                string symbol = String.Empty;
+                string besar = String.Empty;
+                string kecil = String.Empty;
+                string angka = String.Empty;
+                Array.Sort(sort);
+
+                for (int i = 0; i < textBoxInput.TextLength - 1; i++)
+                {
+                    if (char.IsSymbol(sort[i]) == true || sort[i] == '!' || sort[i] =='@' || sort[i] =='#' || sort[i] =='$' || sort[i] =='%' || sort[i] =='^' || sort[i] =='&' || sort[i] =='*' || sort[i] =='(' || sort[i] ==')' || sort[i] =='?' || sort[i] =='.' || sort[i] ==',')
+                    {
+                        symbol = symbol + sort[i];
+                    }
+                    if (char.IsLower(sort[i]) == true)
+                    {
+                        kecil = kecil + sort[i];
+                    }
+                    if (char.IsUpper(sort[i]) == true)
+                    {
+                        besar = besar + sort[i];
+                    }
+                    if (char.IsNumber(sort[i]) == true)
+                    {
+                        angka = angka + sort[i];
+                    }
+
+                }
+                labelOutput.Text = kecil + besar + angka + symbol;
+            }
+        }
+
+        private void labelOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
